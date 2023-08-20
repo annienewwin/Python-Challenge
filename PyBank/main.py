7bcc4a72757daad66915ccfe345fd6c1b9d6c060
annienewwin <102010424+annienewwin@users.noreply.github.com>
2023-08-20 14:09:13 -0700

    Added txt files and small change to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv 
 
+months = []
 profit_losses = []
 
 budget_csv = os.path.join("PyBank","Resources","budget_data.csv")
@@ -11,15 +12,20 @@ with open(budget_csv) as budget_file:
     
     # counting all of the months and putting all of the profit/losses into a list 
     for rows in csv_reader:
+        months.append(rows[0])
         profit_losses.append(rows[1])
-    print(len(profit_losses))
+    total_months = len(months)
+    print(total_months)
 
     # converting the string values to integers 
     profit_losses_convert = [int(i) for i in profit_losses]
-    print(profit_losses)
 
     # calculating the net total of profit/losses over entire period 
     total_profit_losses = sum(profit_losses_convert)
-    print(sum_profit_losses)
+
+    # finding average changes over the entire period 
+    
+
+    # finding the greatest increase in profits with the corresponding month
     
     
